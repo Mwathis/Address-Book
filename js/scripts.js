d5e17768f701f0288cd6c02617ed7bf2c5cc8686
Charles Mwathi <cmwathi24@gmail.com>
2018-05-14 20:29:50 +0300

    Add logic for form submit

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@ class Contact {
     this.phoneNumber=phoneNumber;
   }
 }
+
+$(document).ready(function(){
+  //Define empty array to hold contact objects
+  var allContacts=[];
+
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+    // gets values from input fields
+    var firstName=$("#first-name").val();
+    var lastName=$("#last-name").val();
+    var phoneNumber=$("#phone-number").val();
+
+    // create new contact object
+    var newContact=new Contact(firstName,lastName,phoneNumber);
+    // Add new contact to allContacts array
+    allContacts.push(newContact);
+    // clear values from form input fields
+    //document.getElementById("new-contact").reset();
+    $(this).trigger("reset");
+    $("ul#contacts").append("<li>" +newContact.lastName+ "," +newContact.firstName+"</li>");
+  });
+});
