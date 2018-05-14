2842e8f4728492dd7434cef8e307a00977b4013a
Charles Mwathi <cmwathi24@gmail.com>
2018-05-14 19:46:00 +0300

    Add address book content

---
+++ README.md
@@ -0,0 +1,5 @@
+# Address Book
+Charles Mwathi
+
+## Description
+This is a simple address book built with html, css and javascript to demonstrate OOP javascript functions.
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+class Contact {
+  constructor(firstName,lastName,phoneNumber) {
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.phoneNumber=phoneNumber;
+  }
+}
